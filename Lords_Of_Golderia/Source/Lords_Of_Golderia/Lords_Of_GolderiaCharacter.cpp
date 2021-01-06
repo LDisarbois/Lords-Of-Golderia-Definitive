edb626626064bd35a8326b3ab00b3090a0f208a5
Loïc <loic.disarbois@ynov.com>
2021-01-06 09:33:51 +0100

    push contenu dernier jour

--- Lords_Of_Golderia/Source/Lords_Of_Golderia/Lords_Of_GolderiaCharacter.cpp
+++ Lords_Of_Golderia/Source/Lords_Of_Golderia/Lords_Of_GolderiaCharacter.cpp
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Lords_Of_GolderiaCharacter.h"
-#include "HeadMountedDisplayFunctionLibrary.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
@@ -55,73 +54,18 @@ void ALords_Of_GolderiaCharacter::SetupPlayerInputComponent(class UInputComponen
 {
 	// Set up gameplay key bindings
 	check(PlayerInputComponent);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ALords_Of_GolderiaCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ALords_Of_GolderiaCharacter::MoveRight);
 
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &ALords_Of_GolderiaCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &ALords_Of_GolderiaCharacter::LookUpAtRate);
-
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &ALords_Of_GolderiaCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &ALords_Of_GolderiaCharacter::TouchStopped);
-
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ALords_Of_GolderiaCharacter::OnResetVR);
-}
-
-
-void ALords_Of_GolderiaCharacter::OnResetVR()
-{
-	// If Lords_Of_Golderia is added to a project via 'Add Feature' in the Unreal Editor the dependency on HeadMountedDisplay in Lords_Of_Golderia.Build.cs is not automatically propagated
-	// and a linker error will result.
-	// You will need to either:
-	//		Add "HeadMountedDisplay" to [YourProject].Build.cs PublicDependencyModuleNames in order to build successfully (appropriate if supporting VR).
-	// or:
-	//		Comment or delete the call to ResetOrientationAndPosition below (appropriate if not supporting VR)
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
-}
-
-void ALords_Of_GolderiaCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		Jump();
-}
-
-void ALords_Of_GolderiaCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		StopJumping();
-}
-
-void ALords_Of_GolderiaCharacter::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-}
-
-void ALords_Of_GolderiaCharacter::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
 void ALords_Of_GolderiaCharacter::MoveForward(float Value)
 {
 	if ((Controller != nullptr) && (Value != 0.0f))
 	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Value*BaseSpeed);
+		
+		AddMovementInput(FVector(1.0f, 0.0f, 0.0f), Value*BaseSpeed);
 	}
 }
 
@@ -129,13 +73,6 @@ void ALords_Of_GolderiaCharacter::MoveRight(float Value)
 {
 	if ( (Controller != nullptr) && (Value != 0.0f) )
 	{
-		// find out which way is right
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
-		// get right vector 
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
-		AddMovementInput(Direction, Value*BaseSpeed);
+		AddMovementInput(FVector(0.0f, 1.0f, 0.0f), Value*BaseSpeed);
 	}
 }
